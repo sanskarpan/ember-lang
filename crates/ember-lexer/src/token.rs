701d8e6636912bbcff4453085175d32ce3c0e384
sanskarpan <sanskarpandey2004@gmail.com>
2026-08-12 18:56:22 +0530

    Add crates/ember-lexer/src/token.rs (Phase 13 CLI & REPL)

--- crates/ember-lexer/src/token.rs
+++ crates/ember-lexer/src/token.rs
@@ -1,6 +1,6 @@
 use ember_span::Span;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, serde::Serialize)]
 #[repr(u8)]
 pub enum TokenKind {
     // literals
